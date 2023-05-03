e0946afbc6e384b37cb86927d1120fd60faa2bad
Yigit Er <mr.yigiter222@gmail.com>
2023-05-03 14:00:30 +0300

    library management v1

--- src/main/java/com/yigiter/librarymanagement/repository/BookRepository.java
+++ src/main/java/com/yigiter/librarymanagement/repository/BookRepository.java
@@ -9,13 +9,11 @@ import java.util.Optional;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-
+    // Bu class JpaRepository ile crud işlemlerini Hibernate ile yapılması için oluşturdum
+    // Ancak Jdbc ile Sql sorguları yaptığım için kullnmadım
     Optional<Book> findByTitle(String title);
-
     boolean existsByTitle(String title);
-
     boolean existsByIsbn(String isbn);
-
     Optional<Book> findByIsbn(String isbn);
 
 }
